1a30ad2a1862c6902ecede2f120ab04d8c4418cc
zihath <s.zihath04@gmail.com>
2023-10-26 15:53:55 +0530

    second commit

--- sk3.py
+++ sk3.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y = np.array([0, 0, 0, 1, 1, 1])
+y = np.array([0, 0, 0, 1, 1, 1] aaaaaaaaaaaaaaaaaaaaaaa)
 
 from sklearn.linear_model import LogisticRegression
 
